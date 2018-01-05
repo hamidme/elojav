e84bd09d2ec1f61af690f530304357d05c0ffccc
hamidme <hwaasi@gmail.com>
2018-01-05 21:10:09 +0400

    ex1, Eloqent Javascript

---
+++ ex1.js
@@ -0,0 +1,18 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+// Your code here.
+
+var ash = "#"
+while (ash.length < 8){
+	console.log(ash);
+  	ash+="#";
+} 
